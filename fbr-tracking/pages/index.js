54c5bb6a7e89c408d4e3c8faf798915698dd6776
floresabeirario <info@floresabeirario.pt>
2026-03-03 11:52:39 +0000

    Update index.js

--- fbr-tracking/pages/index.js
+++ fbr-tracking/pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
     <div style={styles.pageWrapper}>
       <Head>
         <title>Status | Flores à Beira-Rio</title>
+        <meta name="robots" content="noindex, nofollow" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="icon" href="/icon.png" type="image/png" />
         <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
